522a4a884db35fec74f41c8758cb504c5ff86f55
jhkim988 <fccva3910@gmail.com>
2022-12-30 01:41:15 +0900

    [Refactor] 글 목록을 스트림 베이스로 변경

--- src/main/java/com/dzspring/app/controller/HomeController.java
+++ src/main/java/com/dzspring/app/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.dzspring.app.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +15,17 @@ import com.dzspring.app.service.PostService;
 
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	private static final List<String> categories = Arrays.asList("notices", "qnas", "generals");
+
 	@Autowired
 	private PostService postService;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		model.addAttribute("notices", postService.getPostList());
-		model.addAttribute("qnas", postService.getPostList());
-		model.addAttribute("generals", postService.getPostList());
+		categories
+			.forEach(category -> model.addAttribute(category, postService.getHeadlineWithCategory(category)));
 		return "home";
 	}
 }
