2fbed853d717c7fdbe3fa69193a80ca01699f25c
jhkim988 <fccva3910@gmail.com>
2022-12-30 01:42:38 +0900

    [Rename] 메인화면 글목록 서비스 메서드 이름 수정

--- src/main/java/com/dzspring/app/service/PostService.java
+++ src/main/java/com/dzspring/app/service/PostService.java
@@ -5,5 +5,5 @@ import java.util.List;
 import com.dzspring.app.entity.Post;
 
 public interface PostService {
-	List<Post> getPostList();
+	List<Post> getHeadlineWithCategory(String category);
 }
---
+++ src/main/java/com/dzspring/app/service/impl/PostServiceImpl.java
@@ -0,0 +1,21 @@
+package com.dzspring.app.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dzspring.app.entity.Post;
+import com.dzspring.app.repository.PostRepository;
+import com.dzspring.app.service.PostService;
+
+public class PostServiceImpl implements PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	@Override
+	public List<Post> getHeadlineWithCategory(String category) {
+		return postRepository.findByCategoryLimit10(category);
+	}
+
+}
