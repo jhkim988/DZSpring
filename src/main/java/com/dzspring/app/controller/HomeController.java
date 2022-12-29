f1b9c147f82e61484b073c3ae857ae683b818e75
jhkim988 <fccva3910@gmail.com>
2022-12-30 00:24:53 +0900

    [Feat] HomeController 추가

---
+++ src/main/java/com/dzspring/app/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.dzspring.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		model.addAttribute("notices", postService.getPostList());
+		model.addAttribute("qnas", postService.getPostList());
+		model.addAttribute("generals", postService.getPostList());
+		return "home";
+	}
+}
