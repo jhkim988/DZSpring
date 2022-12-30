34b2b93cbf5dc2fd2fb9e1a54315c14d73be43d2
jhkim <jinhankim988@gmail.com>
2022-12-30 17:54:27 +0900

    [Fix] Service Annotation 추가

--- src/main/java/com/dzspring/app/service/MemberService.java
+++ src/main/java/com/dzspring/app/service/MemberService.java
@@ -2,8 +2,11 @@ package com.dzspring.app.service;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.dzspring.app.entity.Member;
 
+@Service("memerService")
 public interface MemberService {
 	Optional<Member> login(Member loginInfo);
 }
