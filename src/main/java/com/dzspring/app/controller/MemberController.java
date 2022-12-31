d8955c418e8cc9984a83060770cca80296cba463
jhkim988 <fccva3910@gmail.com>
2022-12-31 13:22:10 +0900

    [Fetch] MemberController login 구현 415 에러 해결, 리턴 타입 ResponseEntity<ResponseMessage> 로 변경 ResponseMessage, StatusEnum 을 inner static class 로 추가 ResponseEntity 는 나중에 따로 분리하여 모든 컨트롤러에서 사용할 예정 API 테스트 하여 정상작동 확인함

--- src/main/java/com/dzspring/app/controller/MemberController.java
+++ src/main/java/com/dzspring/app/controller/MemberController.java
@@ -1,10 +1,15 @@
 package com.dzspring.app.controller;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +20,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dzspring.app.entity.Member;
 import com.dzspring.app.service.MemberService;
 
+import lombok.Data;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-
+	
+	@Data
+	private static class ResponseMessage {
+		private StatusEnum status;
+		private String message;
+		private Object object;
+	}
+	
+	private static enum StatusEnum {
+		OK(200, "OK")
+		, BAD_REQUEST(400, "BAD_REQUEST")
+		, NOT_FOUND(404, "NOT_FOUND")
+		, INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR");
+		
+		int status;
+		String code;
+		
+		StatusEnum(int status, String code) {
+			this.status = status;
+			this.code = code;
+		}
+	}
+	
 	@Autowired
 	private MemberService memberService;
 
-	@RequestMapping("/login")
-	public String login(@RequestBody Member loginInfo, final Model model, HttpServletRequest request) {
+	@RequestMapping(value="/login")
+	public ResponseEntity<ResponseMessage> login(@RequestBody Member loginInfo, final Model model, HttpServletRequest request) {
 		Optional<Member> member = memberService.login(loginInfo);
 		member.ifPresent(m -> request.getSession().setAttribute("member", m));
-		return "home";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+		
+		ResponseMessage message = new ResponseMessage();
+		message.setStatus(StatusEnum.OK);
+		message.setMessage("로그인 성공!");
+		
+		return new ResponseEntity<>(message, headers, HttpStatus.OK);
 	}
 
 	@RequestMapping("/logout")
@@ -36,12 +73,13 @@ public class MemberController {
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String view(@RequestBody String id) {
-		return "";
+	public Member view(HttpServletRequest request) {
+		return (Member) request.getSession().getAttribute("member");
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String register(@RequestBody Member registerInfo) {
+//		memberService.register(registerInfo);
 		return "";
 	}
 
