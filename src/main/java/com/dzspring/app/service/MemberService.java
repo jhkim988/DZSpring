fbc581a368258ebe42b3bf9aa90441d21cb5bae5
jhkim988 <fccva3910@gmail.com>
2023-01-01 21:55:13 +0900

    [Fetch] MemberService 구현

--- src/main/java/com/dzspring/app/service/MemberService.java
+++ src/main/java/com/dzspring/app/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.dzspring.app.service;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -9,4 +10,11 @@ import com.dzspring.app.entity.Member;
 @Service("memerService")
 public interface MemberService {
 	Optional<Member> login(Member loginInfo);
+	boolean register(Member registerInfo);
+	Optional<Member> update(Member updateInfo);
+	boolean validate(String id, String pwd);
+	boolean delete(String id) throws SQLException;
+	boolean hasMember(String type, String value);
+	Optional<Member> findId(String method, String value);
+	boolean initPwd(String id);
 }
--- src/main/java/com/dzspring/app/service/impl/MemberServiceImpl.java
+++ src/main/java/com/dzspring/app/service/impl/MemberServiceImpl.java
@@ -1,18 +1,102 @@
 package com.dzspring.app.service.impl;
 
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dzspring.app.entity.Member;
+import com.dzspring.app.repository.MemberRepository;
 import com.dzspring.app.service.MemberService;
 
 @Service("memberServiceImpl")
 public class MemberServiceImpl implements MemberService {
 
+	@Autowired
+	MemberRepository memberRepository;
+
 	@Override
 	public Optional<Member> login(Member loginInfo) {
-		return Optional.of(null);
+		Member member = memberRepository.findOneById(loginInfo.getId());
+		if (member == null || member.getPwd().equals(loginInfo.getPwd())) {
+			member = null;
+		}
+		return Optional.ofNullable(member);
 	}
 
+	@Override
+	public boolean register(Member registerInfo) {
+		return 1 == memberRepository.insert(registerInfo);
+	}
+
+	@Override
+	public Optional<Member> update(Member updateInfo) {
+		int update = memberRepository.update(updateInfo);
+		if (update == 0)
+			return Optional.ofNullable(null);
+		return Optional.of(memberRepository.findOneById(updateInfo.getId()));
+	}
+
+	@Override
+	public boolean validate(String id, String pwd) {
+		Member member = memberRepository.findOneById(id);
+		if (member == null)
+			return false;
+		return member.getPwd().equals(pwd);
+	}
+
+	@Override
+	@Transactional
+	public boolean delete(String id) throws SQLException {
+		int move = memberRepository.insertTmpTable(id);
+		int delete = memberRepository.delete(id);
+		if (1 != move || 1 != delete)
+			throw new SQLException();
+		return true;
+	}
+
+	@Override
+	public boolean hasMember(String type, String value) {
+		Member result = null;
+		if ("id".equals(type))
+			memberRepository.findOneById(value);
+		else if ("phone".equals(type))
+			memberRepository.findOneByPhone(value);
+		else if ("email".equals(type))
+			memberRepository.findOneByEmail(value);
+		else
+			throw new UnsupportedOperationException();
+		return result != null;
+	}
+
+	@Override
+	public Optional<Member> findId(String method, String value) {
+		Member result = null;
+		if ("phone".equals(method))
+			result = memberRepository.findOneByPhone(value);
+		else if ("email".equals(method))
+			result = memberRepository.findOneByEmail(value);
+		else
+			throw new UnsupportedOperationException();
+		return Optional.ofNullable(result);
+	}
+
+	@Override
+	public boolean initPwd(String id) {
+		Member member = memberRepository.findOneById(id);
+		if (member != null)	return false;
+		
+		Map<String, Object> tmp = memberRepository.getTmpPwd(id);
+		String tmpPwd = (String) tmp.get("tmpPwd");
+		Timestamp createdAt = (Timestamp) tmp.get("createdAt");
+		Timestamp before5min = Timestamp.valueOf(LocalDateTime.now().minusMinutes(10));
+		if (tmpPwd == null || createdAt == null || createdAt.after(before5min))	return false;
+
+		return memberRepository.update(member) == 1;
+	}
 }
