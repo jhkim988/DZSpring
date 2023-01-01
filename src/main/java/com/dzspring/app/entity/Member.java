fc1395abb5464cedd5f14e3b77c4af09e77e522a
jhkim988 <fccva3910@gmail.com>
2023-01-01 22:03:15 +0900

    [Fetch] Member Entity 에 Jackson annotation 추가 JSON 통신 시 비밀번호를 숨길 수 있고, 날짜 포맷을 지정함

--- src/main/java/com/dzspring/app/entity/Member.java
+++ src/main/java/com/dzspring/app/entity/Member.java
@@ -2,6 +2,9 @@ package com.dzspring.app.entity;
 
 import java.sql.Timestamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +16,14 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Member {
 	private String id;
+	@JsonIgnore
 	private String pwd;
 	private String name;
 	private String email;
 	private String phone;
+	@JsonFormat(pattern="yyyy년 MM월 dd일")
 	private Timestamp createdAt;
+	@JsonFormat(pattern="yyyy년 MM월 dd일")
 	private Timestamp updatedAt;
 	private Integer authority;
 }
