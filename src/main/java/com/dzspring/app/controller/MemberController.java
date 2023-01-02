54caf524f7cec8318bfa0630ed22c414e5e3f56a
jhkim <jinhankim988@gmail.com>
2023-01-02 16:58:01 +0900

    [Fetch] @PathVariable -> @RequestBody 로 일부 변경 URL 인코딩으로 이메일 등의 값을 처리하기 까다로운 이유로 변경함 + DB 연동 테스트 모두 수행

--- src/main/java/com/dzspring/app/controller/MemberController.java
+++ src/main/java/com/dzspring/app/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.dzspring.app.controller;
 
+import static com.dzspring.app.controller.ResponseMessage.getJSONHeader;
+
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,21 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dzspring.app.entity.Member;
 import com.dzspring.app.service.MemberService;
-import static com.dzspring.app.controller.ResponseMessage.*;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
 	@Autowired
 	private MemberService memberService;
 
-	@RequestMapping(value="/login")
+	@RequestMapping(value = "/login")
 	public ResponseEntity<ResponseMessage> login(@RequestBody Member loginInfo, final Model model,
 			HttpServletRequest request) {
 		Optional<Member> member = memberService.login(loginInfo);
-		member.ifPresent(m -> request.getSession().setAttribute("member", m));
-		ResponseMessage message = new ResponseMessage("로그인 성공!");
+		ResponseMessage message = new ResponseMessage("로그인 실패!");
+		member.ifPresent(m -> {
+			request.getSession().setAttribute("member", m);
+			message.setMessage("로그인 성공!");
+		});
 		return new ResponseEntity<>(message, getJSONHeader(), HttpStatus.OK);
 	}
 
@@ -54,7 +63,6 @@ public class MemberController {
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<ResponseMessage> register(@RequestBody Member registerInfo, HttpServletRequest request) {
 		boolean result = memberService.register(registerInfo);
-
 		ResponseMessage message = new ResponseMessage(result ? "회원 가입 성공!" : "일시적 오류");
 		message.setData(result);
 		if (result)
@@ -77,12 +85,18 @@ public class MemberController {
 		return new ResponseEntity<>(message, getJSONHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value="/validate")
-	public ResponseEntity<ResponseMessage> validate(@RequestBody String inputPwd, HttpServletRequest request) {
+	@RequestMapping(value = "/validate")
+	public ResponseEntity<ResponseMessage> validate(@RequestBody HashMap<String, String> input,
+			HttpServletRequest request) {
+		String inputPwd = input.get("pwd");
 		HttpSession session = request.getSession();
 		Member member = (Member) session.getAttribute("member");
-		boolean result = memberService.validate(member.getId(), inputPwd);
 		ResponseMessage message = new ResponseMessage();
+		if (member == null) {
+			message.setData(Boolean.valueOf(false));
+			return new ResponseEntity<>(message, getJSONHeader(), HttpStatus.METHOD_NOT_ALLOWED);
+		}
+		boolean result = memberService.validate(member.getId(), inputPwd);
 		if (result)
 			session.setAttribute("validate", true);
 		message.setData(result);
@@ -93,12 +107,13 @@ public class MemberController {
 	public ResponseEntity<ResponseMessage> delete(@PathVariable String id, HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		Object isValidate = session.getAttribute("validate");
-		if (isValidate != null && isValidate.getClass() == boolean.class && (boolean) isValidate) {
+		if (isValidate != null && isValidate.getClass() == Boolean.class && (Boolean) isValidate) {
 			boolean result = false;
 			ResponseMessage message = new ResponseMessage("회원 탈퇴");
 			try {
 				result = memberService.delete(id);
 			} catch (SQLException e) {
+				e.printStackTrace();
 				message.setMessage("일시적 오류");
 			}
 			message.setData(result);
@@ -111,18 +126,23 @@ public class MemberController {
 		}
 	}
 
-	@RequestMapping(value = "/hasMember/{type}/{value}")
-	public ResponseEntity<ResponseMessage> hasMember(@PathVariable("type") String type,
-			@PathVariable("value") String value) {
+	@RequestMapping(value = "/hasMember")
+	public ResponseEntity<ResponseMessage> hasMember(@RequestBody HashMap<String, String> queryInfo) {
+		String type = queryInfo.get("type");
+		String value = queryInfo.get("value");
+		if (type == null || value == null) {
+			return new ResponseEntity<>(null, getJSONHeader(), HttpStatus.BAD_REQUEST);
+		}
 		boolean result = memberService.hasMember(type, value);
-		ResponseMessage message = new ResponseMessage(result ? "이미 사용 중입니다." : "");
+		ResponseMessage message = new ResponseMessage(result ? "이미 사용 중입니다." : "사용 가능합니다.");
 		message.setData(result);
 		return new ResponseEntity<>(message, getJSONHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/findId/{method}/{value}")
-	public ResponseEntity<ResponseMessage> findId(@PathVariable("method") String method,
-			@PathVariable("value") String value) {
+	@RequestMapping(value = "/findId")
+	public ResponseEntity<ResponseMessage> findId(@RequestBody HashMap<String, String> queryInfo) {
+		String method = queryInfo.get("method");
+		String value = queryInfo.get("value");
 		Optional<Member> result = memberService.findId(method, value);
 		ResponseMessage message = new ResponseMessage("일치하는 정보가 없습니다.");
 		message.setData(result.isPresent());
