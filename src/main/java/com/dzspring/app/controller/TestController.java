751de79ab64269ef3e1d1a81b93286ee4e606976
jhkim <jinhankim988@gmail.com>
2023-01-02 17:45:55 +0900

    [Fetch] 테스트용 회원 생성 컨트롤러 구현

---
+++ src/main/java/com/dzspring/app/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.dzspring.app.controller;
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dzspring.app.entity.Member;
+import com.dzspring.app.service.MemberService;
+import static com.dzspring.app.controller.ResponseMessage.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping("/generateMember")
+	public ResponseEntity<ResponseMessage> generateMember() {
+		for (int i = 0; i < 100; i++) {
+			memberService.register(Member.builder()
+					.id("user"+i)
+					.pwd("pwd")
+					.name("자바좋아"+i)
+					.sex("M")
+					.email("java" + i + "@naver.com")
+					.phone("0100000000"+i)
+					.birth(Date.valueOf("1900-01-01")
+)					.build());
+		}
+		return new ResponseEntity<>(null, getJSONHeader(), HttpStatus.OK);
+	}
+}
