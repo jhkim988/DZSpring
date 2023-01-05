f0de2986ef653848e107741799727e39566630d0
jhkim <jinhankim988@gmail.com>
2023-01-05 20:01:45 +0900

    [Fetch] 검색 제안 기능 구현

--- src/main/webapp/WEB-INF/views/common/search.jsp
+++ src/main/webapp/WEB-INF/views/common/search.jsp
@@ -1,8 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<input type="text"/>
-<ul>
-	<li>검색 제안 리스트</li>
-	<li>검색 제안 리스트</li>
-	<li>검색 제안 리스트</li>
+<script type="text/javascript" src=<c:url value="/resources/js/common/search.js"/>></script>
+<form id="searchForm">
+	<label>
+		<select id="searchType">
+			<option value="title" class="str">제목</option>
+			<option value="category" class="str">카테고리</option>
+			<option value="author" class="str">저자</option>
+		</select>
+	</label>
+	<input id="searchText" type="text"/>
+	<input type="submit" value="검색"/>
+</form>
+
+<ul id="suggest">
+	<li style="display: none; border: 1px solid black">
+		<img src="#"/>
+		<p class="title"></p>
+		<p class="author"></p>
+	</li>
 </ul>
---
+++ src/main/webapp/resources/js/common/search.js
@@ -0,0 +1,34 @@
+const main = () => {
+	const suggestList = suggest.querySelector("li").cloneNode(true);
+	searchText.addEventListener("keyup", async e => {
+		if (searchText.value == '') {
+			suggest.replaceChild();
+			return;
+		}
+		const response = await fetch(`${context.value}goods/search`, {
+			method: `POST`
+			, headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			}
+			, body: JSON.stringify({
+				method: searchType.value
+				, value: {
+					value: searchText.value
+				}
+			})
+		});
+		const json = await response.json();
+		suggest.replaceChildren();
+		json.data.forEach(d => {
+			const copy = suggestList.cloneNode(true);
+			d.img = d.img || `default`;
+			copy.querySelector("img").src = `${context.value}goods/thumbnail/${d.img}`
+			copy.querySelector(".title").textContent = d.title;
+			copy.querySelector(".author").textContent = d.author;
+			copy.style.display = "list-item"
+			suggest.appendChild(copy);
+		});
+	});
+}
+
+window.onload = main;
