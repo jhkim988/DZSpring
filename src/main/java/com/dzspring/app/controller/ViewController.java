62be93d84a3620e41ac39957722d919a433487c8
jhkim <jinhankim988@gmail.com>
2023-01-05 20:58:03 +0900

    [Fetch] 상품 상세보기 구현

--- src/main/java/com/dzspring/app/controller/ViewController.java
+++ src/main/java/com/dzspring/app/controller/ViewController.java
@@ -1,14 +1,33 @@
 package com.dzspring.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dzspring.app.service.GoodsService;
 
 @Controller
 @RequestMapping("/view")
 public class ViewController {
 
+	private final GoodsService goodsService;
+	
+	@Autowired
+	public ViewController(GoodsService goodsService) {
+		this.goodsService = goodsService;
+	}
+	
 	@RequestMapping("/member")
 	public String viewMember() {
 		return "/view/member";
+	}
+	
+	@RequestMapping("/goods/{id}")
+	public ModelAndView viewGoods(@PathVariable int id) {
+		ModelAndView mav = new ModelAndView("/view/goods");
+		mav.addObject("goods", goodsService.findOneById(id));
+		return mav;
 	}
 }
--- src/main/webapp/WEB-INF/views/view/goods.jsp
+++ src/main/webapp/WEB-INF/views/view/goods.jsp
@@ -1,12 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<h1>${ goods.title }</h1>
+<c:choose>
+	<c:when test="${ goods.img != null }">
+		<img src=<c:url value="/goods/thumbnail/${ goods.img }"/>/>
+	</c:when>
+	<c:otherwise>
+		<img src=<c:url value="/goods/thumbnail/default"/>/>
+	</c:otherwise>
+</c:choose>
 
-</body>
-</html>
+<p>${ goods.author }</p>
+<p>${ goods.publisher }</p>
+<p>${ goods.price }</p>
+<p>${ goods.publishedAt }</p>
+<p>${ goods.totalPage }</p>
+<p>${ goods.statusCode }</p>
+<p>${ goods.intro }</p>
+<p>${ goods.authorIntro }</p>
+<p>${ goods.publisherIntro }</p>
+<p>${ goods.recommendation }</p>
