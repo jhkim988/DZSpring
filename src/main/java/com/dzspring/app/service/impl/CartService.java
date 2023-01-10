aa40021f80588490c87bc33be8b26c1c0be44986
jhkim <jinhankim988@gmail.com>
2023-01-10 17:44:55 +0900

    [Fetch] Cart id리스트로 Cart 객체 리스트 얻는 메서드 구현

--- src/main/java/com/dzspring/app/service/impl/CartService.java
+++ src/main/java/com/dzspring/app/service/impl/CartService.java
@@ -1,5 +1,6 @@
 package com.dzspring.app.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,8 +20,18 @@ public class CartService {
 		this.cartRepository = cartRepository;
 	}
 	
-	public List<Cart> list(Map<String, Object> map) {
-		return cartRepository.list(map);
+	/**
+	 * @param map 에 memberId key 가 있어야 한다.
+	 * @return 해당 member의 cart 목록을 반환한다.
+	 */
+	public List<Cart> listByMember(Map<String, Object> map) {
+		return cartRepository.listByMember(map);
+	}
+	
+	public List<Cart> findByIdList(List<Integer> ids) {
+		Map<String, List<Integer>> map = new HashMap<>();
+		map.put("list", ids);
+		return cartRepository.findByIdList(map);
 	}
 	
 	public boolean insert(Cart cart) {
