ee3571d8c2317f75e2e011d70de1c316211d2454
jhkim <jinhankim988@gmail.com>
2023-01-10 17:31:16 +0900

    [Fetch] 상품 구매 트랜잭션 구현

--- src/main/java/com/dzspring/app/service/impl/OrderService.java
+++ src/main/java/com/dzspring/app/service/impl/OrderService.java
@@ -3,41 +3,60 @@ package com.dzspring.app.service.impl;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dzspring.app.entity.Order;
+import com.dzspring.app.entity.OrderItem;
+import com.dzspring.app.repository.OrderItemRepository;
 import com.dzspring.app.repository.OrderRepository;
 
 @Service
 public class OrderService {
 
 	private final OrderRepository orderRepository;
-	
+	private final OrderItemRepository orderItemRepository;
+	private final CartService cartService;
+
 	@Autowired
-	public OrderService(OrderRepository orderRepository) {
+	public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository,
+			CartService cartService) {
 		this.orderRepository = orderRepository;
+		this.orderItemRepository = orderItemRepository;
+		this.cartService = cartService;
 	}
-	
+
 	public List<Order> list(Map<String, Object> map) {
 		return orderRepository.list(map);
 	}
-	
-	public boolean insert(Order order) {
-		return 1 == orderRepository.insert(order);
-	}
-	
-	public boolean update(Order order) {
-		return 1 == orderRepository.update(order);
-	}
-	
+
 	public boolean delete(int id) {
 		return 1 == orderRepository.delete(id);
 	}
-	
+
 	public Optional<Order> findOneById(int id) {
 		return Optional.ofNullable(orderRepository.findOneById(id));
 	}
-	
+
+	public List<OrderItem> findOrderItemByOrderId(int orderId) {
+		return orderItemRepository.findItemsByOrderId(orderId);
+	}
+
+	@Transactional(propagation=Propagation.REQUIRED, rollbackFor = {Exception.class})
+	public boolean order(Order order, List<Integer> cartIds) {
+		int orderId = orderRepository.insert(order);
+		return orderItemRepository.insertItems(
+				cartService
+					.findByIdList(cartIds).stream()
+					.map(cartItem -> OrderItem.builder()
+						.orderId(orderId)
+						.goodsId(cartItem.getGoodsId())
+						.quantity(cartItem.getQuantity())
+						.build())
+					.collect(Collectors.toList())) > 0;
+	}
 }
