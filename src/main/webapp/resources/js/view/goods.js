3ea87415273662feb9364d580520b75aaf518ca5
jhkim <jinhankim988@gmail.com>
2023-01-10 17:36:42 +0900

    [Fetch] 상품 상세보기에서 구매 화면으로 이동 구현

--- src/main/webapp/resources/js/view/goods.js
+++ src/main/webapp/resources/js/view/goods.js
@@ -19,5 +19,23 @@ const main = () => {
 			alert(`일시적 오류`);
 		}
 	});
+	
+	insertOrder.addEventListener("click", async e => {
+		alert(`장바구니에 추가 후 구입화면으로 넘어갑니다.`);
+		const response = await fetch(`${context.value}order/goodsViewTo`, {
+			method: `POST`
+			, headers: {
+				'Content-Type': `application/json;charset=utf-8`
+			}
+			, body: JSON.stringify({
+				goodsId: goodsId.value
+				, quantity: quantity.value
+			})
+		});
+		const json = await response.json();
+		if (json.data) {
+			location.href = json.url;
+		}
+	});
 }
 window.onload = main;
