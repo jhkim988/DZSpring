0b62ea6a2941dc9db7ce9ce0b1c0312b1b09cf1e
jhkim <jinhankim988@gmail.com>
2023-01-11 14:04:18 +0900

    [Fetch] 주문 데이터 추가 컨트롤러 구현

--- src/main/java/com/dzspring/app/controller/TestController.java
+++ src/main/java/com/dzspring/app/controller/TestController.java
@@ -1,6 +1,11 @@
 package com.dzspring.app.controller;
 
+import static com.dzspring.app.controller.ResponseMessage.getJSONHeader;
+
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,20 +13,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dzspring.app.entity.Cart;
 import com.dzspring.app.entity.Member;
+import com.dzspring.app.entity.Order;
 import com.dzspring.app.service.MemberService;
-import static com.dzspring.app.controller.ResponseMessage.*;
+import com.dzspring.app.service.impl.CartService;
+import com.dzspring.app.service.impl.OrderService;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 	
-	
 	private final MemberService memberService;
+	private final OrderService orderService;
+	private final CartService cartService;
 	
 	@Autowired
-	public TestController(MemberService memberService) {
+	public TestController(MemberService memberService, OrderService orderService, CartService cartService) {
 		this.memberService = memberService;
+		this.orderService = orderService;
+		this.cartService = cartService;
 	}
 	
 	@RequestMapping("/generateMember")
@@ -36,6 +47,32 @@ public class TestController {
 					.phone("0100000000"+i)
 					.birth(Date.valueOf("1900-01-01")
 )					.build());
+		}
+		return new ResponseEntity<>(null, getJSONHeader(), HttpStatus.OK);
+	}
+	
+	@RequestMapping("/generateOrder")
+	public ResponseEntity<ResponseMessage> generateOrder() {
+		ThreadLocalRandom rand = ThreadLocalRandom.current();
+		for (int i = 0; i < 100; i++) {
+			List<Integer> list = new ArrayList<>();
+			for (int j = 0; j < 3; j++) {
+				Cart cart = Cart.builder()
+								.memberId("user"+i)
+								.goodsId(337+j)
+								.quantity(rand.nextInt(5))
+								.build();
+				cartService.insert(cart);
+				list.add(cart.getId());
+			}
+			orderService.order(Order.builder()
+					.memberId("user" + i)
+					.receiverName("자바좋아"+i)
+					.receiverPhone("0100000000"+i)
+					.address("혜화역 4번출구")
+					.payMethod("외상")
+					.status("배송 준비중")
+					.build(), list);
 		}
 		return new ResponseEntity<>(null, getJSONHeader(), HttpStatus.OK);
 	}
