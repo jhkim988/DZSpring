27e312c35a3a621f514c8f13199755def57507d5
jhkim <jinhankim988@gmail.com>
2023-01-11 11:21:29 +0900

    [Fetch] Cart insert 를 변경에 따른 수정

--- src/main/java/com/dzspring/app/controller/CartController.java
+++ src/main/java/com/dzspring/app/controller/CartController.java
@@ -62,7 +62,7 @@ public class CartController {
 		cart.setMemberId(member.getId());
 		ResponseMessage message = new ResponseMessage();
 		Map<String, Object> data = new HashMap<>();
-		boolean result = cartService.insert(cart);
+		boolean result = cartService.insert(cart) > 0;
 		data.put("result", result);
 		if (result) {
 			data.put("url", ResponseMessage.path("/view/cart"));
