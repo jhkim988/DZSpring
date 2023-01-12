74fef15eac00eb38ada7d30035577e0a4d9b203f
jhkim <jinhankim988@gmail.com>
2023-01-12 10:46:52 +0900

    [Fetch] 허용된 컬럼만 수정하는 Order update 메서드 구현

--- src/main/java/com/dzspring/app/repository/OrderRepository.java
+++ src/main/java/com/dzspring/app/repository/OrderRepository.java
@@ -8,6 +8,7 @@ import com.dzspring.app.entity.Order;
 public interface OrderRepository {
 	int insert(Order order);
 	int update(Order order);
+	int updateReceiverInfo(Order order);
 	int delete(int id);
 	List<Order> list(Map<String, Object> map);
 	Order findOneById(int id);
